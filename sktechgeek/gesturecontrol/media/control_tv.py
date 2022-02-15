82b630ee91240bec7f65999382461a02d77b746f
sandeep3028 <35072657+sandeep3028@users.noreply.github.com>
2022-02-15 12:32:57 -0500

    boolean added to limit the connections to mqtt broker in while loop

--- sktechgeek/gesturecontrol/media/control_tv.py
+++ sktechgeek/gesturecontrol/media/control_tv.py
@@ -17,6 +17,11 @@ show = sys.argv[1]
 broker = 'homeassistant'
 topic = 'home-assistant/media/options'
 
+play = False
+pause = False
+stop = False
+
+
 while cap.isOpened():
     success, image = cap.read()
     image = cv2.flip(image, 1)
@@ -25,17 +30,29 @@ while cap.isOpened():
     gesture = hand_tracking.get_gesture(lm_list)
 
     if gesture == 'palm':
-        cv2.putText(image, "Play", (20, 70), cv2.FONT_HERSHEY_PLAIN, 3,
+        if not play:
+            play = True
+            pause = False
+            stop = False
+            cv2.putText(image, "Play", (20, 70), cv2.FONT_HERSHEY_PLAIN, 3,
                     (0, 0, 0), 3)
-        publish.single(topic, 'play', hostname=broker, auth={'username': "mqtt-user", 'password': "Mqtt.50786"})
+            publish.single(topic, 'play', hostname=broker, auth={'username': "mqtt-user", 'password': "Mqtt.50786"})
     elif gesture == 'upward_fist':
-        cv2.putText(image, "Pause", (20, 70), cv2.FONT_HERSHEY_PLAIN, 3,
+        if not pause:
+            play = False
+            pause = True
+            stop = False
+            cv2.putText(image, "Pause", (20, 70), cv2.FONT_HERSHEY_PLAIN, 3,
                     (0, 0, 0), 3)
-        publish.single(topic, 'pause', hostname=broker, auth={'username': "mqtt-user", 'password': "Mqtt.50786"})
+            publish.single(topic, 'pause', hostname=broker, auth={'username': "mqtt-user", 'password': "Mqtt.50786"})
     elif gesture == 'straight_fist':
-        cv2.putText(image, "Stop", (20, 70), cv2.FONT_HERSHEY_PLAIN, 3,
+        if not stop:
+            play = False
+            pause = False
+            stop = True
+            cv2.putText(image, "Stop", (20, 70), cv2.FONT_HERSHEY_PLAIN, 3,
                     (0, 0, 0), 3)
-        publish.single(topic, 'stop', hostname=broker, auth={'username': "mqtt-user", 'password': "Mqtt.50786"})
+            publish.single(topic, 'stop', hostname=broker, auth={'username': "mqtt-user", 'password': "Mqtt.50786"})
 
     if show != 'no':
         cv2.imshow("Media Control", image)
